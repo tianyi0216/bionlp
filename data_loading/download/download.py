6421a48fd8a0ac229b086c768d074777c186c881
tianyi0216 <tianyixu.iii@gmail.com>
2025-01-29 10:39:49 -0600

    fixed download

--- data_loading/download/download.py
+++ data_loading/download/download.py
@@ -4,9 +4,9 @@ import requests
 class BaseDownloader:
     """Base class for downloading datasets."""
     
-    def __init__(self, dataset_name, url, save_dir="clinical_trials_data"):
+    def __init__(self, dataset_name, urls, save_dir="clinical_trials_data"):
         self.dataset_name = dataset_name
-        self.url = url
+        self.urls = urls
         self.save_dir = save_dir
         self.file_path = os.path.join(save_dir, f"{dataset_name}.zip")
 
@@ -21,11 +21,27 @@ class BaseDownloader:
         
         print(f"⬇️ Downloading {self.dataset_name} dataset...")
 
-        response = requests.get(self.url, stream=True)
-        with open(self.file_path, "wb") as file:
-            for chunk in response.iter_content(chunk_size=1024):
-                if chunk:
-                    file.write(chunk)
-
-        print(f"✅ {self.dataset_name} downloaded successfully!")
-        return self.file_path
+        # Download all urls
+        success_urls = []
+        failed_urls = []
+        for url in self.urls:
+            try:
+                response = requests.get(url, stream=True)
+                response.raise_for_status()  # Raise an exception for bad status codes
+                
+                with open(self.file_path, "wb") as file:
+                    for chunk in response.iter_content(chunk_size=1024):
+                        if chunk:
+                            file.write(chunk)
+                
+                print(f"✅ {self.dataset_name} downloaded successfully!")
+                success_urls.append(url)
+                
+            except requests.RequestException as e:
+                print(f"⚠️ Failed to download from {url}: {str(e)}")
+                failed_urls.append(url)
+                continue
+        
+        print(f"✅ {self.dataset_name} downloaded successfully from {len(success_urls)} URLs")
+        print(f"❌ Failed to download from {len(failed_urls)} URLs")
+        return success_urls, failed_urls
